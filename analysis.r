28213720aa74bc594404b1497fd5b7b41a073233
Allison Duprey <ahduprey@live.unc.edu>
2018-07-11 15:07:11 -0400

    loading google sheet for non-UNC frass data (to compare with milk jug frass); created new data frame with NCBG filter paper data

--- analysis.r
+++ analysis.r
@@ -1,5 +1,4 @@
-# Script for analyzing frass data
-
+# Script for analyzing milk jug frass data
 frassLoad = function(open = T, write = F) {
   require(gsheet)
   url = "https://docs.google.com/spreadsheets/d/1RwXzwhHUbP0m5gKSOVhnKZbS1C_NrbdfHLglIVCzyFc/edit#gid=806965256"
@@ -13,7 +12,47 @@ frassLoad = function(open = T, write = F) {
   if (open) { return (data) }
 }
 
-#removing outliers 
+# Script for analyzing filter paper data
+library(gsheet)
+library(dplyr)
+library(tidyr)
+
+# Function for reading in frass data from GoogleDoc
+# *if aim is to backup GoogleDoc and write to disk only, then open =F and write = T
+# *if aim is to use data without writing to disk, then open = T and write = F
+
+frassData = function(open = F, write = F) {
+  require(gsheet)
+  url = "https://docs.google.com/spreadsheets/d/1RwXzwhHUbP0m5gKSOVhnKZbS1C_NrbdfHLglIVCzyFc/edit#gid=1479231778"
+  data = gsheet2tbl(url)
+  
+  if (write) {
+    # Write a copy
+    write.csv(data, paste('data/frass_', Sys.Date(), '.csv', sep = ''),
+              row.names = F)
+  }
+  if (open) { return (data) }
+}
+
+
+# Function that takes a date field (formatted as %m/%d/%Y) and a time field
+# (hh:mm in 24h time), converts the date to julian day and adds the fractional
+# day represented by the hours and minutes
+
+julianDayTime = function(date, hour_min) {
+  require(lubridate)
+  jday = yday(date)
+  temp = sapply(strsplit(hour_min, ":"), function(x) {
+    x = as.numeric(x)
+    x[1] + x[2]/60
+  })
+  output = jday + temp/24
+  return(output)
+}
+
+NCBG_PR_frassdata = frassData(open = T)
+
+#removing outliers in frassLoad
 data = frassLoad(open = T)
 dataWO = data[data$Weight_Raw < 50,]
 data_rawpcsWO = data[data$Pieces_Raw < 60,]
@@ -60,9 +99,24 @@ summary(raw_img)
 
 # Img_raw vs.Img_sort
 plot(data$Img_Raw[data$Img_Sorted<20], data$Img_Sorted[data$Img_Sorted<20], main = "Comparison Img_Raw vs. Img_Sorted (% of area estimate)", xlab = "Raw Img.", ylab ="Sorted Img.", col = 'violet', pch = 20)
-rawsort_img = lm(data_img_exlc_outlier$Img_Sorted ~ data_img_exlc_outlier$Img_Raw, data = data_img_exlc_outlier)
+rawsort_img = lm(data$Img_Sorted ~ data$Img_Raw, data = data)
 abline(rawsort_img)
-summary(rawsort_img)
+rawsort_img_sum = summary(rawsort_img)
+r2 = rawsort_img_sum$adj.r.squared
+rawsort_img_pval = rawsort_img_sum$coefficients[2,4]
+mylabel = bquote(italic(R)^2 == .(format(r2, digits = 3)))
+text(x = 11.5, y = 16, labels = mylabel)
+
+#NCBG Comparison: Filter paper vs. Milk jug collection. Traps set on 3rd.
+#Filter paper collected on the 6th & 10th
+#milk jug collected on 10th
+#must sum filter paper frass per circle to make accurate comparison
+filterpaper = NCBG_PR_frassdata[c(1155:1170,1183:1198),]
+
+#plot comparing sorted pcs
+
+#plot comparing sorted weight
+
 
 # COMPARISONS TO DO
 
