154f137c7137471d2f60ad591b887b3c0db7ace0
Savannah <savcart@ad.unc.edu>
2026-02-04 11:12:40 -0500

    added Nosa's amazing code for updating fulldataset and temp stuff :)

--- Frass_2026.R
+++ Frass_2026.R
@@ -8,6 +8,70 @@ library(dplyr)
 library(tidyr)
 library(purrr)
 library(rstatix)
+library(tidyverse)
+library(jsonlite)
+library(daymetr)
+
+# CC data ----
+options(timeout = 300)  
+
+api_url <- "https://api.github.com/repos/hurlbertlab/caterpillars-analysis-public/contents/data"
+files <- fromJSON(api_url)
+
+dataset_file <- files$name[grepl("fullDataset", files$name, ignore.case = TRUE)]
+
+# pick the latest one
+latest_file <- dataset_file[1]
+
+github_raw <- "https://raw.githubusercontent.com/hurlbertlab/caterpillars-analysis-public/master/data/"
+
+fullDataset <- read.csv(paste0(github_raw, latest_file))
+
+
+
+
+
+#----------------------------------------------------------------------------------
+library(daymetr)
+
+tmp_file = tempfile(fileext = ".csv")
+
+AnomalySites = fullDataset[, c("Name","Year", "Latitude", "Longitude")] %>% 
+  filter(Name %in% c("NC Botanical Garden", "Prairie Ridge Ecostation")) %>% 
+  group_by(Name, Latitude, Longitude) %>% 
+  summarise(n = n()) %>% 
+  select(-n) %>% as.data.frame()   
+
+AnomalyDaymetr = AnomalySites %>% 
+  rename(
+    site = Name,
+    lat = Latitude,
+    lon = Longitude
+  ) %>%
+  write.csv(tmp_file, row.names = FALSE)
+
+
+# pass temp CSV to function
+TempAnomaly = download_daymet_batch(
+  file_location = tmp_file,
+  start = 2015,
+  end = 2024, # this is the most recent available in daymetr
+  internal = TRUE
+)
+
+# remove temporary file 
+unlink(tmp_file)
+
+
+TempAnomalyData_clean <- lapply(TempAnomaly, function(x) {
+  x$data %>% mutate(site = x$site,
+                    Latidue = x$latitude,
+                    Longitude = x$longitude)
+})
+
+AllTempAnomaly= bind_rows(TempAnomalyData_clean)
+
+
 
 #--------------------------------------------------------------------------------------------------------------------------------
 # reading in frassdata and then functions for correcting time and date
