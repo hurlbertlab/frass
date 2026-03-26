83e85937e4c11228e6ce4d8331b64687a7b82fb5
Savannah <savcart@ad.unc.edu>
2026-03-26 14:16:25 -0400

    started new file so i can do wet leaf stuff

---
+++ Frass_wetleaves.R
@@ -0,0 +1,70 @@
+# Looking at wet leaf data and impact on bug abundance
+#Savannah Carter
+#4.4.2
+
+#libraries
+library(gsheet)
+library(dplyr)
+library(tidyr)
+library(purrr)
+library(rstatix)
+library(tidyverse)
+library(jsonlite)
+library(daymetr)
+library(gridExtra)
+library(zoo)
+
+# *+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+
+#   Datasets needed:
+# *+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+
+
+#Loading in CC data 
+options(timeout = 300)  
+api_url <- "https://api.github.com/repos/hurlbertlab/caterpillars-analysis-public/contents/data"
+files <- fromJSON(api_url)
+dataset_file <- files$name[grepl("fullDataset", files$name, ignore.case = TRUE)]
+# pick the latest one
+latest_file <- dataset_file[1]
+github_raw <- "https://raw.githubusercontent.com/hurlbertlab/caterpillars-analysis-public/master/data/"
+fullDataset <- read.csv(paste0(github_raw, latest_file))
+
+#----------------------------------------------------------------------------------
+#loading in temp data
+tmp_file = tempfile(fileext = ".csv")
+
+AnomalySites = fullDataset[, c("Name","Year", "Latitude", "Longitude")] %>% 
+  filter(Name %in% c("NC Botanical Garden", "Prairie Ridge Ecostation")) %>% 
+  group_by(Name, Latitude, Longitude) %>% 
+  summarise(n = n()) %>% 
+  select(-n) %>% as.data.frame()   
+
+AnomalyDaymetr = AnomalySites %>% 
+  rename(
+    site = Name,
+    lat = Latitude,
+    lon = Longitude
+  ) %>%
+  write.csv(tmp_file, row.names = FALSE)
+
+# pass temp CSV to function
+TempAnomaly = download_daymet_batch(
+  file_location = tmp_file,
+  start = 2015,
+  end = 2024, # this is the most recent available in daymetr
+  internal = TRUE)
+# remove temporary file 
+unlink(tmp_file)
+
+TempAnomalyData_clean <- lapply(TempAnomaly, function(x) {
+  x$data %>% mutate(site = x$site,
+                    Latidue = x$latitude,
+                    Longitude = x$longitude)})
+
+AllTemp= bind_rows(TempAnomalyData_clean) #this is the file we want (has all NCBG and PR data)
+
+
+
+
+
+
+
