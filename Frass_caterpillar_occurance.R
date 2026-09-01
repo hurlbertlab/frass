28ac5ce73ce33612705fd3341243a68ca7e4c9ae
Savannah <savcart@ad.unc.edu>
2026-09-01 12:04:13 -0400

    saving progress on occuarnce doc

---
+++ Frass_caterpillar_occurance.R
@@ -0,0 +1,267 @@
+## comparing frass and caterpillar occurrence (#traps with frass >0.1mg.d and #of surveys with caterpillars present)
+#Savannah Carter
+#4.4.2
+
+#libraries
+library(gsheet)
+library(dplyr)
+library(tidyr)
+library(purrr)
+library(rstatix)
+library(tidyverse)
+library(jsonlite)
+library(daymetr)
+
+# *+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+
+#   reading in CC and altering it per julian week :
+# *+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+
+
+#Loading in CC data 
+options(timeout = 300)  
+api_url <- "https://api.github.com/repos/hurlbertlab/caterpillars-analysis-public/contents/data"
+files <- fromJSON(api_url)
+dataset_file <- files$name[grepl("fullDataset", files$name, ignore.case = TRUE)]
+# pick the latest one
+latest_file <- dataset_file[1]
+github_raw <- "https://raw.githubusercontent.com/hurlbertlab/caterpillars-analysis-public/master/data/"
+fullDataset <- read.csv(paste0(github_raw, latest_file))
+
+
+
+
+
+
+
+
+
+
+
+
+
+# *+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+
+#   reading in frass and altering it per julian week :
+# *+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+
+
+# Function for reading in frass data from GoogleDoc
+# *if aim is to backup GoogleDoc and write to disk only, then open =F and write = T
+# *if aim is to use data without writing to disk, then open = T and write = F
+frassData = function(open = T, write = F) {
+  require(gsheet)
+  url = "https://docs.google.com/spreadsheets/d/1RwXzwhHUbP0m5gKSOVhnKZbS1C_NrbdfHLglIVCzyFc/edit#gid=1479231778"
+  data = gsheet2tbl(url)
+  
+  if (write) {
+    # Write a copy
+    write.csv(data, paste('data/frass_', Sys.Date(), '.csv', sep = ''),
+              row.names = F)
+  }
+  if (open) { return (data) }
+}
+#----------------------------------------------------------------------------------
+#Function for fixing time format and downloading corrected csv
+TimeCleaning = function() {
+  read_in_data <- gsheet2tbl('https://docs.google.com/spreadsheets/d/1RwXzwhHUbP0m5gKSOVhnKZbS1C_NrbdfHLglIVCzyFc/edit#gid=1479231778')
+  
+  remove_NAs <- read_in_data %>%
+    filter(!is.na(Time.Set) & !is.na(Time.Collected))
+  
+  write.csv(remove_NAs %>% 
+              mutate(Time.Set = ifelse(test = grepl(":", remove_NAs$Time.Set), 
+                                       yes = remove_NAs$Time.Set, 
+                                       no = paste(substr(remove_NAs$Time.Set, 1, nchar(remove_NAs$Time.Set)-2), ":", substr(remove_NAs$Time.Set, 3, 4), sep = "")), 
+                     Time.Collected = ifelse(test = grepl(":", remove_NAs$Time.Collected), 
+                                             yes = remove_NAs$Time.Collected, 
+                                             no = paste(substr(remove_NAs$Time.Collected, 1, nchar(remove_NAs$Time.Collected)-2), ":", substr(remove_NAs$Time.Collected, 3, 4), sep = ""))), 
+            paste('data/frass_', Sys.Date(), '.csv', sep = ''), row.names = F)
+}
+#----------------------------------------------------------------------------------
+# Function that takes a date field (formatted as %m/%d/%Y) and a time field (hh:mm in 24h time), converts the date to julian day and adds the fractional
+julianDayTime = function(date, hour_min) {
+  require(lubridate)
+  jday = yday(date)
+  temp = sapply(strsplit(hour_min, ":"), function(x) { #(day represented by the hours and minutes)
+    x = as.numeric(x)
+    x[1] + x[2]/60
+  })
+  output = jday + temp/24
+  return(output)
+}
+#-----------------------------------------------------------------------------------------------
+# altering frassdata so that times and days are corrected
+data = frassData(open = T) %>%
+  filter(!is.na(Time.Set) & !is.na(Time.Collected)) %>%
+  mutate(Date.Set = as.Date(Date.Set, format = "%m/%d/%Y"),
+         Time.Set = as.character(Time.Set),
+         Time.Collected = as.character(Time.Collected),
+         Date.Collected = as.Date(Date.Collected, format = "%m/%d/%Y"),
+         Year = format(Date.Collected, "%Y"),
+         jday.Set = julianDayTime(Date.Set, Time.Set),
+         jday.Collected = julianDayTime(Date.Collected, Time.Collected),
+         frass.mg.d = Frass.mass..mg./(jday.Collected - jday.Set),
+         frass.no.d = Frass.number/(jday.Collected - jday.Set),
+         jday = (floor(jday.Collected) + floor(jday.Set))/2)
+
+#-----------------------------------------------------------------------------------------------
+#filter data so only reliable rows are left then filter frass.mg.d so that only traps with total mass >0.1 are left
+filtered_mass <- data %>%
+  filter(OK==1)%>%
+  filter(Frass.mass..mg. > 0.1) #question: to filter by frass.mg.d or frass.mass..mg? also > or >=?
+
+
+
+
+#-----------------------------------------------------------------------------------------------
+# using data to find mean frass per day for reliable frass only 
+#read in proper url, change dates and label events for below meanfrass
+url = "https://docs.google.com/spreadsheets/d/1RwXzwhHUbP0m5gKSOVhnKZbS1C_NrbdfHLglIVCzyFc/edit#gid=1611171427"
+events = gsheet2tbl(url)
+events$date = as.Date(events$date, format = "%m/%d/%Y")
+
+meanfrass = data %>%
+  filter(!is.na(Frass.mass..mg.)) %>%
+  filter(OK == 1) %>% #keeps reliable frass row
+  mutate(site = as.character(ifelse(Site=="Botanical Garden", 8892356, 117))) %>%
+  group_by(site, Date.Collected, Year, jday) %>%
+  summarize(
+    mass = mean(frass.mg.d, na.rm = TRUE),
+    .groups = "drop"
+  ) %>%
+  mutate( #altering the density based on size of the trap
+    trap_area_cm2 = ifelse(Year <= 2018, 309.74, 197.71), #before 2018 use 309cm^2 after use 197cm^2 
+    density_mg_cm2 = mass / trap_area_cm2, #trap adjusted
+    density_mg_m2 = density_mg_cm2 * 10000   # optional but recommended
+  ) %>%
+  left_join(events[, c('date', 'site', 'reliability')],
+            by = c('Date.Collected' = 'date', 'site' = 'site')) %>%
+  rename(date = Date.Collected)
+
+#--------------------------------------------------------------------------------
+#make sure frass has correct columns:
+#having mean Frass data sorted by julian week
+meanfrass <- meanfrass %>%
+  mutate(julianweek = 7 * floor(jday / 7) + 4)%>% #this is using jday calculated from 'data' where it uses day and time set to alter jday
+  mutate(Year = as.integer(Year)) #make sure integer
+
+#combine and average meanfrass data that comes from same week (2015-2023 this happened), is mean frass per day
+meanfrass_combinedweeks <- meanfrass %>%
+  group_by(site, Year, julianweek) %>%
+  summarise(
+    # average frass measurements
+    mass = mean(mass, na.rm = TRUE),
+    trap_corrected_mass = mean(density_mg_cm2, na.rm = TRUE),
+    # keep representative values for the rest
+    date = min(date, na.rm = TRUE),   # or first(date)
+    jday = mean(jday, na.rm = TRUE),
+    reliability = first(reliability),
+    .groups = "drop")
+
+
+
+
+
+
+
+
+
+
+
+
+
+#--------------------------------------------------------------------------------------------------------------------------------
+#Meandensitybyweek function for caterpillar data
+# Function for calculating the mode of a series of values
+# --in this particular use case, if there multiple modes, we want the largest value
+Mode = function(x){ 
+  if (!is.numeric(x)) {
+    stop("values must be numeric for mode calculation")
+  }
+  ta = table(x)
+  tam = max(ta)
+  mod = as.numeric(names(ta)[ta == tam])
+  return(max(mod))
+}
+
+# Function for substituting values based on a condition using dplyr::mutate
+# Modification of dplyr's mutate function that only acts on the rows meeting a condition
+mutate_cond <- function(.data, condition, ..., envir = parent.frame()) {
+  condition <- eval(substitute(condition), .data, envir)
+  .data[condition, ] <- .data[condition, ] %>% mutate(...)
+  .data
+}
+
+# Function for calculating and displaying arthropod phenology by week (density, mean biomas)
+meanDensityByWeek = function(surveyData, # merged dataframe of Survey and arthropodSighting tables for a single site
+                             ordersToInclude = 'All',       # which arthropod orders to calculate density for (codes)
+                             
+                             minLength = 0,         # minimum arthropod size to include 
+                             jdRange = c(1,365),
+                             outlierCount = 10000,
+                             plot = FALSE,
+                             plotVar = 'fracSurveys', # 'meanDensity' or 'fracSurveys' or 'meanBiomass'
+                             minSurveyCoverage = 0.8, # minimum proportion of unique survey branches examined per week in order to include the week as a data point
+                             allDates = TRUE,
+                             new = TRUE,
+                             color = 'black',
+                             allCats = TRUE,
+                             ...)                  
+
+{
+  
+  if(length(ordersToInclude)==1 & ordersToInclude[1]=='All') {
+    ordersToInclude = unique(surveyData$Group)
+  }
+  
+  numUniqueBranches = length(unique(surveyData$PlantFK))
+  
+  firstFilter = surveyData %>%
+    filter(julianday >= jdRange[1], julianday <= jdRange[2]) %>%
+    mutate(julianweek = 7*floor(julianday/7) + 4)
+  
+  effortByWeek = firstFilter %>%
+    group_by(julianweek) %>%
+    summarize(nSurveyBranches = n_distinct(PlantFK),
+              nSurveys = n_distinct(ID)) %>%
+    mutate(modalBranchesSurveyed = Mode(5*ceiling(nSurveyBranches/5)),
+           nSurveySets = nSurveys/modalBranchesSurveyed,
+           modalSurveySets = Mode(round(nSurveySets)),
+           okWeek = ifelse(nSurveySets/modalSurveySets >= minSurveyCoverage, 1, 0))
+  
+  if (allDates) {
+    effortByWeek$okWeek = 1
+  }
+  
+  if (!allCats) {
+    secondFilter = firstFilter %>%
+      filter(Hairy != 1, Tented != 1, Rolled != 1)
+  } else {
+    secondFilter = firstFilter
+  }
+  
+  arthCount = secondFilter %>%
+    filter(Length >= minLength, 
+           Group %in% ordersToInclude) %>%
+    mutate(Quantity2 = ifelse(Quantity > outlierCount, 1, Quantity)) %>% #outlier counts replaced with 1
+    group_by(julianweek) %>%
+    summarize(totalCount = sum(Quantity2, na.rm = TRUE),
+              numSurveysGTzero = length(unique(ID[Quantity > 0])),
+              totalBiomass = sum(Biomass_mg, na.rm = TRUE)) %>% 
+    right_join(effortByWeek, by = 'julianweek') %>%
+    filter(okWeek == 1) %>%
+    #next line replaces 3 fields with 0 if the totalCount is NA
+    mutate_cond(is.na(totalCount), totalCount = 0, numSurveysGTzero = 0, totalBiomass = 0) %>%
+    mutate(meanDensity = totalCount/nSurveys,
+           fracSurveys = 100*numSurveysGTzero/nSurveys,
+           meanBiomass = totalBiomass/nSurveys) %>%
+    arrange(julianweek) %>%
+    data.frame()
+  
+  if (plot & new) {
+    plot(arthCount$julianweek, arthCount[, plotVar], type = 'l', 
+         col = color, las = 1, ...)
+    points(arthCount$julianweek, arthCount[, plotVar], pch = 16, col = color, ...)
+  } else if (plot & new==F) {
+    points(arthCount$julianweek, arthCount[, plotVar], type = 'l', col = color, ...)
+    points(arthCount$julianweek, arthCount[, plotVar], pch = 16, col = color, ...)
+  }
+  return(arthCount)
+}
